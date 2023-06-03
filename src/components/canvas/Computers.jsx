5cecada0b45102f8203e455591f2d1b754a4e1c1
Cannigia Laluw <cannigialaluw@gmail.com>
2023-06-02 22:26:18 -0700

    rendered animated object

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,47 @@
+/* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, Stage, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
-	return <div>Computers</div>;
+import Retro from "./Retro";
+
+// const Computer = () => {
+// 	const { scene } = useGLTF("/retro-transformed.glb");
+
+// 	return (
+// 		<mesh>
+// 			<hemisphereLight intensity={0.15} groundColor="black" />
+// 			<pointLight intensity={1} />
+// 			<primitive object={scene} />
+// 		</mesh>
+// 	);
+// };
+
+const ComputersCanvas = () => {
+	return (
+		<Canvas
+			frameloop="demand"
+			shadows
+			camera={{ position: [20, 3, 5], fov: 25 }}
+			gl={{
+				preserveDrawingBuffer: true,
+			}}
+		>
+			<Stage environment="city" intensity={0.6}>
+				<Suspense fallback={null}>
+					<OrbitControls
+						enableZoom={false}
+						maxPolarAngle={Math.PI / 2}
+						minPolarAngle={Math.PI / 2}
+					/>
+					<Retro />
+				</Suspense>
+			</Stage>
+			<Preload all />
+		</Canvas>
+	);
 };
 
-export default Computers;
+export default ComputersCanvas;

d058762952f13c703e0df1f094f7a55bafbfc039
Cannigia Laluw <cannigialaluw@gmail.com>
2023-06-04 14:43:48 -0700

    added About section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,12 +17,26 @@ const Hero = () => {
 						Hi, I am <span className="text-[#915eff]">Canni</span>
 					</h1>
 					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
-						A Front end developer juadlknsdlk nsdjnvlw adnflskdnvkjsfnolsf
-						jsndlknsldg
+						A Front end developer.
 					</p>
 				</div>
 			</div>
 			<ComputersCanvas />
+			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+				<a href="#about">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+						<motion.div
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: "loop",
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						/>
+					</div>
+				</a>
+			</div>
 		</section>
 	);
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,25 +1,58 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, Stage, useGLTF } from "@react-three/drei";
-
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
+// import Retro from "./Retro";
 
-import Retro from "./Retro";
+const Retro = ({ isMobile }) => {
+	const { scene } = useGLTF("/retro_computer/scene.gltf");
 
-// const Computer = () => {
-// 	const { scene } = useGLTF("/retro-transformed.glb");
-
-// 	return (
-// 		<mesh>
-// 			<hemisphereLight intensity={0.15} groundColor="black" />
-// 			<pointLight intensity={1} />
-// 			<primitive object={scene} />
-// 		</mesh>
-// 	);
-// };
+	return (
+		<mesh>
+			<hemisphereLight intensity={1.2} groundColor="green" />
+			<pointLight intensity={1} />
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={0.12}
+				penumbra={1}
+				intensity={1}
+			/>
+			<primitive
+				object={scene}
+				position={[0, -3.25, -1.5]}
+				scale={isMobile ? 2.0 : 4.5}
+				rotation={[-0.01, -0.2, -0.1]}
+			/>
+		</mesh>
+	);
+};
 
 const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		// Add event listener for when the screen size changes
+		const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+		// Set the initial value of IsMobile
+		setIsMobile(mediaQuery.matches);
+
+		// Define callback funtion to handles changes to media query
+		const handleMediaQueryChange = (e) => {
+			setIsMobile(e.matches);
+		};
+
+		// Add callback function as listener for changes to media query
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+		// removes listener after the component is unmounted
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
+
 	return (
 		<Canvas
 			frameloop="demand"
@@ -29,16 +62,16 @@ const ComputersCanvas = () => {
 				preserveDrawingBuffer: true,
 			}}
 		>
-			<Stage environment="city" intensity={0.6}>
-				<Suspense fallback={null}>
-					<OrbitControls
-						enableZoom={false}
-						maxPolarAngle={Math.PI / 2}
-						minPolarAngle={Math.PI / 2}
-					/>
-					<Retro />
-				</Suspense>
-			</Stage>
+			{/* <Stage environment="city" intensity={0.6}> */}
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Retro isMobile={isMobile} />
+			</Suspense>
+			{/* </Stage> */}
 			<Preload all />
 		</Canvas>
 	);
